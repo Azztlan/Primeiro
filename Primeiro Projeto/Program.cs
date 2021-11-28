d043c9b36d952d43a11ed7364b26f76c3c4fdb96
Azztlan <honorio.andy@gmail.com>
2021-11-28 15:52:46 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro Projeto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Primeiro
+{
+               
+      class Program
+      { 
+      
+        static void Main(string[] args)
+        {
+            var a = "Hello ";
+            var b = "World!";
+            var c = " Meu nome é";
+            var d = " Anderson Honório";
+            Console.WriteLine(a + b + c + d);
+
+        }
+
+      }       
+             
+    
+}
